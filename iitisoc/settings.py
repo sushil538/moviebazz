d529a8a8677e39d29cf879517827caada619b36e
Sushil <yadavsushil538@gmail.com>
2022-08-13 02:41:01 +0530

    add domain

--- iitisoc/settings.py
+++ iitisoc/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--*++bs!(lb#+t)it9@3njv%rt67i%72ia&_4i$l$qoox5m9i%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','https://moviebazzer.herokuapp.com/']
 
 
 # Application definition
